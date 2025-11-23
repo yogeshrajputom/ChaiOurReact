71a38dbb692bab2f74a78bbfd656d87fda83fc64
Yogesh Rajput <153723012+yogeshrajputom@users.noreply.github.com>
2025-11-23 07:16:23 +0000

    basics react

---
+++ reactBasics/src/App.jsx
@@ -0,0 +1,15 @@
+import Chai from "./Chai"
+
+
+function App() {
+
+
+  return (
+    <>
+      <Chai />
+      <h1>Hello Guys kiya hal chal</h1>
+    </>
+  )
+}
+
+export default App
---
+++ reactBasics/src/Chai.jsx
@@ -0,0 +1,11 @@
+function Chai(){
+    return(
+        <>
+        <h1>
+            this is ready
+        </h1>
+        </>
+    )
+}
+
+export default Chai;
