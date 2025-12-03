73de420d2abd7396f789b954a9348d1c8d721232
[yogeshrajputom] <[yogeshrajputom@gmail.com]>
2025-12-03 12:48:54 +0530

    create and test how to work React Router

---
+++ reactRouter/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className='text-white justify-center items-center text-center'>
+fgfs
+    </div>
+    </>
+  )
+}
+
+export default App
